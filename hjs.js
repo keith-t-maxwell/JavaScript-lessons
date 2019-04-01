5124570822c426f4fac9b1b396573b9b9e0bf757
keith-t-maxwell <46784452+keith-t-maxwell@users.noreply.github.com>
2019-04-01 21:16:07 +0300

    #4-задание

---
+++ hjs.js
@@ -0,0 +1,12 @@
+
+var arr = [1,2,3,4,5];
+for (var i=0; i < arr.length;i++) {
+	document.write(arr[i] + '<br>');
+} 
+
+var result = 1;
+var arc = [2,3,4,5];
+for (var i=0; i < arc.length;i++) {
+	result = result * arc[i];
+}
+alert(result + ' Задача №3');
