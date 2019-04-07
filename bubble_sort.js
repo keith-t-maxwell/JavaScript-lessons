2d10521dadf4443f10fe47dca1bfc6c23ee1a61e
keith-t-maxwell <46784452+keith-t-maxwell@users.noreply.github.com>
2019-04-07 16:32:26 +0300

    #5

---
+++ bubble_sort.js
@@ -0,0 +1,19 @@
+ array = [5,7,3,4,8,11,13,43,55,1,2] 
+	
+ function bubble_sort(arr){
+	 var len = arr.length;
+	 for (i = 0; i < len; i++){
+		 for (j = 0; j < len-1-i; j++){
+			 if(arr[j+1]<A[j]){
+				 var t = arr[j+1];
+				 arr[j+1] = arr[j];
+				 arr[j] = t;
+			 }
+		 }
+	 }
+	 return arr;
+ }
+ 
+ function(array);
+ 
+ console.log(arr);
