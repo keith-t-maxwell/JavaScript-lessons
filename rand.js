3caac8d15b54a938ce9e70505738c5dd85bc2b21
keith-t-maxwell <46784452+keith-t-maxwell@users.noreply.github.com>
2019-04-09 17:15:06 +0300

    #5-random_game

--- rand.js
+++ rand.js
@@ -1,10 +1,10 @@
- namber = Math.floor(Math.random() * 200); 
+ namber = Math.floor(Math.random() * 100); 
  
- a = prompt('Первый игрок, введите любое число до 200:'); 
- b = prompt('Второй игрок, введите любое число до 200:');
+ a = prompt('Первый игрок, введите любое число до 100:'); 
+ b = prompt('Второй игрок, введите любое число до 100:');
  
- function NEAR(na,nb) {
-	 var a,b,max,min,namber,na,nb; 
+ function NEAR(na,nb){
+	 var a,b,maxa,maxb,mina,minb,na,nb,minn,maxn; 
 	 maxa = Math.max(namber,a);
 	 maxb = Math.max(namber,b);
 	 mina = Math.min(namber,a);
@@ -13,11 +13,12 @@
 	 nb = maxb - minb; //разница между макс-мин пары б-число
 	 minn = Math.min(na,nb); //минимальный результат пред. раунда
 	 maxn = Math.max(na,nb); //максимальный результат пред. раунда
-	 if(na < nb) {
-		 alert('Первый игрок выиграл, загаданное число - ' + namber + '');
+	 
+	 if(na < nb){
+		 alert('Первый игрок выиграл, загаданное число - ' + namber);
 	 } 
-	 if(nb < na) {
-		 alert('Второй игрок выиграл, загаданное число - ' + namber + '');
+	 else{
+		 alert('Второй игрок выиграл, загаданное число - ' + namber);
 	 } 
 	 
  } 
